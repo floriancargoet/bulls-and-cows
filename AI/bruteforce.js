96701e29167e25f9962ba1791dbb678e9d799776
Florian Cargoët <florian.cargoet@gmail.com>
2013-01-21 14:37:39 +0100

    added 'letters' mode

--- AI/bruteforce.js
+++ AI/bruteforce.js
@@ -1,8 +1,13 @@
+/*jshint  node:true */
 
 var Game = require('../game.js');
- 
-var g = new Game(4);
- 
+
+var gd = new Game({
+    length : 4,
+    mode : 'digits',
+    debug : true
+});
+
 // brute force method
 var count = 0;
 for (var i = 123; i < 10000; i++) {
@@ -16,14 +21,45 @@ for (var i = 123; i < 10000; i++) {
             unique = false;
         }
     });
-    
+
     if (unique) {
         count++;
-        console.log(s);
-        var result = g.guess(s);
+        var result = gd.guess(s);
         if (result.won) {
             console.log('Found', s, 'in', count, 'guesses');
             break;
+        }
+    }
+}
+
+
+var gl = new Game({
+    length : 4,
+    mode : 'letters',
+    debug : true
+});
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+
+// brute force method
+var count = 0, s;
+var i, j, k, l;
+outerloop:
+for (i = 0; i < 26; i++) {
+    for (j = 0; j < 26; j++) {
+        if (i === j) continue;
+        for (k = 0; k < 26; k++) {
+            if (i === k || j === k) continue;
+            for (l = 0; l < 26; l++) {
+                if (i === l || j === l || k === l) continue;
+                s = letters[i] + letters[j] + letters[k] + letters[l];
+                count++;
+                console.log(s);
+                var result = gl.guess(s);
+                if (result.won) {
+                    console.log('Found', s, 'in', count, 'guesses');
+                    break outerloop;
+                }
+            }
         }
     }
 }
--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 /*jshint  node:true */
 
-var Game = function (n, debug) {
-    this.length = n;
-    this.goal = generate(n);
-    if (debug) {
+var Game = function (options) {
+    this.goal = generate(options);
+    this.length = options.length;
+    if (options.debug) {
         console.log('String to guess', this.goal);
     }
 };
 
-Game.prototype.guess = function guess(s) {
+Game.prototype.guess = function (s) {
     var l = this.length;
 
     if (typeof s !== 'string' || s.length !== l) {
@@ -33,13 +33,41 @@ Game.prototype.guess = function guess(s) {
     return result;
 };
 
-function generate(n) {
+function generate(options) {
+    switch (options.mode) {
+        case 'digits' :
+            return generateDigits(options);
+        case 'letters' :
+            return generateLetters(options);
+    }
+}
+
+function generateDigits(options) {
+    var n = options.length;
     if (n > 10) {
         throw new Error('I cannot generate a string of length ' + n + ' because digits must be unique');
     }
     var s = '';
     while (n--) {
         var c = String(Math.floor(Math.random() * 10));
+        if (s.indexOf(c) === -1) {
+            s += c;
+        } else {
+            n++;
+        }
+    }
+    return s;
+}
+
+var letters = 'abcdefghijklmnopqrstuvwxyz';
+function generateLetters(options) {
+    var n = options.length;
+    if (n > 26) {
+        throw new Error('I cannot generate a string of length ' + n + ' because letters must be unique');
+    }
+    var s = '';
+    while (n--) {
+        var c = letters[Math.floor(Math.random() * 26)];
         if (s.indexOf(c) === -1) {
             s += c;
         } else {
